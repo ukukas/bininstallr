a4b0037765f20e27a8fa8254cdcab2d167e9d3d4
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2022-01-11 19:18:00 -0500

    add uninstall script

---
+++ uninstall.ps1
@@ -0,0 +1,15 @@
+#Requires -RunAsAdministrator
+
+$sitelib = Join-Path $env:ProgramData "r-site-library"
+
+Remove-Item $sitelib -Recurse -Force -ErrorAction "Stop"
+
+Get-ChildItem (Join-Path $env:SystemDrive "Users") -Force -Directory `
+-Exclude "All Users","Default User","Public" | ForEach-Object {
+    $renviron = Join-Path $_.FullName "Documents\.Renviron"
+    if (Test-Path $renviron) {
+        $content = Get-Content $renviron
+        $content | Where-Object {$_ -notmatch "R_LIBS_SITE"} |
+        Set-Content -Path $renviron -Force
+    }
+}
