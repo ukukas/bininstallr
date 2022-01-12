cff8d9f29d20cab633d81cdaf3b3843cae48e881
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2022-01-11 19:46:44 -0500

    create serial install script

---
+++ sitelibinstallr-serial.ps1
@@ -0,0 +1,58 @@
+#Requires -RunAsAdministrator
+
+$pkgroot = Resolve-Path ".\packages" -ErrorAction "Stop"
+$sitelib = Join-Path $env:ProgramData "r-site-library"
+$rversion = 4.1
+
+$installdir = Join-Path $sitelib $rversion
+
+if (-not (Test-Path $installdir)) {
+    New-Item $installdir -ItemType "directory" -Force | Out-Null
+}
+
+Join-Path $pkgroot $rversion |
+Get-ChildItem -Filter "*.zip" -ErrorAction "Stop" |
+ForEach-Object {
+    Expand-Archive $_.FullName -DestinationPath $installdir -Force
+    $pkgname = $_.Name.Split("_")[0]
+    $pkgdir =  Join-Path $installdir $pkgname
+    $md5file = Join-Path $pkgdir "MD5"
+    $success = $true
+    if (Test-Path $md5file) {
+        Get-Content $md5file | ForEach-Object {
+            $hash = $_.Split(" ")[0]
+            $file = Join-Path $pkgdir $_.Split(" ")[1].TrimStart("*")
+            if (Test-Path $file) {
+                if ((Get-FileHash $file -Algorithm "MD5").Hash -ne $hash) {
+                    Set-Variable -Name "success" -Value $false
+                }
+            } else { Set-Variable -Name "success" -Value $false }
+        }
+    } else { Set-Variable -Name "success" -Value $false }
+    if ($success) {
+        Write-Host "$pkgname successfully unpacked and MD5 sums checked"
+    } else {
+        Remove-Item $pkgdir -Recurse -Force -ErrorAction "SilentlyContinue"
+        Write-Warning "$pkgname failed checks and was not installed"
+    }
+}
+
+$purged = $false
+Get-ChildItem (Join-Path $env:SystemDrive "Users") -Force -Directory `
+-Exclude "All Users","Default User","Public" | ForEach-Object {
+    $renviron = Join-Path $_.FullName "Documents\.Renviron"
+    if (-not (Test-Path $renviron)) {
+        New-Item $renviron -ItemType "file" | Out-Null
+    } else {
+        $old = Get-Content $renviron
+        $new = $old | Where-Object {$_ -notmatch "^R_LIBS_SITE="} |
+        Set-Content -Path $renviron -Force -PassThru
+    }
+    if ($old.Count -gt $new.Count) {
+        Set-Variable -Name "purged" -Value $true
+    }
+    Add-Content $renviron -Value "R_LIBS_SITE=`"$sitelib\%v`""
+}
+if ($purged) {
+    Write-Warning "existing R_LIBS_SITE entries removed from .Renviron files"
+}
